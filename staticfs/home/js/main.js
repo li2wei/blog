dac918cef2c5874d6aef26899dd0a6e6cc8f5c5e
Lijiang0703 <li_jiang0703@outlook.com>
2016-06-09 01:13:02 +0800

    painel和底部导航

--- staticfs/home/js/main.js
+++ staticfs/home/js/main.js
@@ -3,10 +3,11 @@
  */
 $(window).on('load',function () {
     var _init = new init();
-    $('.navtitle').find('.fa-bars').on('click',_init.toggleMenu)
+    $('.navtitle').find('.fa-bars').on('click',_init.toggleMenu);
     // document.on('mouse',function () {
     //     $('.menu').css('display','none');
     // });
+    $('.pannel').find('.like').on('click',_init.onlike);
 
 });
 var init = function (options) {
@@ -17,5 +18,8 @@ var init = function (options) {
 init.prototype = {
     toggleMenu:function () {
         $('.menu').toggle();
+    },
+    onlike:function () {
+        $('.pannel').find('.like a').addClass('onlike').find('i').removeClass('fa-heart-o').addClass('fa-heart');
     }
 }
