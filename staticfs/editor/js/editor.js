4f396dd307e9a2f94f304fed7ea520ef1276f348
Lijiang0703 <li_jiang0703@outlook.com>
2016-06-14 18:31:33 +0800

    markdown引入

---
+++ staticfs/editor/js/editor.js
@@ -0,0 +1,45 @@
+require(deps,function (editormd) {
+    $(document).ready(function () {
+        // editormd.loadCSS("/staticfs/thirdparty/Markdown/lib/codemirror/addon/fold/foldgutter");
+        // $.get('../editor/js/test.md', function(md) {
+            testEditor = editormd("test-editormd", {
+                width: "90%",
+                height: 640,
+                path: '/staticfs/thirdparty/Markdown/lib/',   //css引用路径
+                markdown: '',
+                codeFold: true,
+                searchReplace: true,
+                saveHTMLToTextarea: true,                // 保存HTML到Textarea
+                htmlDecode: "style,script,iframe|on*",       // 开启HTML标签解析，为了安全性，默认不开启
+                emoji: true,
+                taskList: true,
+                tex: true,
+                tocm: true,         // Using [TOCM]
+                autoLoadModules: false,
+                previewCodeHighlight: true,
+                flowChart: true,
+                sequenceDiagram: true,
+                //dialogLockScreen : false,   // 设置弹出层对话框不锁屏，全局通用，默认为true
+                //dialogShowMask : false,     // 设置弹出层对话框显示透明遮罩层，全局通用，默认为true
+                //dialogDraggable : false,    // 设置弹出层对话框不可拖动，全局通用，默认为true
+                //dialogMaskOpacity : 0.4,    // 设置透明遮罩层的透明度，全局通用，默认值为0.1
+                //dialogMaskBgColor : "#000", // 设置透明遮罩层的背景颜色，全局通用，默认为#fff
+                imageUpload: true,
+                imageFormats: ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
+                imageUploadURL: "./php/upload.php",
+                onload: function () {
+                    console.log('onload', this);
+                    //this.fullscreen();
+                    //this.unwatch();
+                    //this.watch().fullscreen();
+                    //this.setMarkdown("#PHP");
+                    //this.width("100%");
+                    //this.height(480);
+                    //this.resize("100%", 640);
+                }
+            });
+        // })
+    });
+
+
+});
--- staticfs/thirdparty/config/config.js
+++ staticfs/thirdparty/config/config.js
@@ -1,23 +1,45 @@
-var  path = {
-    'jquery': 'thirdparty/jquery/jquery.min',
-    'text':  'thirdparty/requirejs/text',
-    'backbone': 'thirdparty/backbone/backbone-0.9.10',
-    'underscore': 'thirdparty/backbone/underscore-1.4.4',
-    'bootstrap': 'thirdparty/bootstrap/bootstrap.min',
+var path = {
+    'jquery': 'jquery/jquery.min',
+    'text': 'requirejs/text',
+    'backbone': 'backbone/backbone-0.9.10',
+    'underscore': 'Markdown/lib/underscore.min',
+    'bootstrap': 'bootstrap/bootstrap.min',
     // 'mo':'thirdparty/mojs/mo'
-    'mo':'thirdparty/mojs-master/build/mo.min'
+    'mo': 'mojs-master/build/mo.min',
+    'editor': 'Markdown/editormd.min',
+    'marked': "Markdown/lib/marked.min",
+    'prettify': "Markdown/lib/prettify.min",
+    'raphael': "Markdown/lib/raphael.min",
+    'flowchart': "Markdown/lib/flowchart.min",
+    'jqueryflowchart': "Markdown/lib/jquery.flowchart.min",
+    'sequenceDiagram': "Markdown/lib/sequence-diagram.min",
+    'katex': "//cdnjs.cloudflare.com/ajax/libs/KaTeX/0.1.1/katex.min",
+    'editormd': "Markdown/editormd.amd" // Using Editor.md amd version for Req
 };
 var shim = {
     'backbone': {
         deps: ['underscore', 'jquery'],
         exports: 'Backbone'
     },
-    'bootstrap':{
-        deps:['jquery']
+    'bootstrap': {
+        deps: ['jquery']
+    },
+    'editor': {
+        deps: ['jquery']
+    },
+    'jqueryflowchart':{
+        deps: ['jquery']
+    },
+    'sequenceDiagram':{
+        deps:['raphael']
     }
+    // 'flowchart':{
+    //     deps: ['jquery']
+    // }
 };
 require.config({
-    baseUrl:'/staticfs/',
-    paths:path,
-    shim:shim
+    baseUrl: '/staticfs/thirdparty',
+    paths: path,
+    shim: shim,
+    waitSeconds: 30
 });
