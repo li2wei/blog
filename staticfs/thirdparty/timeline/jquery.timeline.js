bc0520bc48965700103578e1ae5018353fccf981
Lijiang0703 <li_jiang0703@outlook.com>
2016-07-31 11:03:55 +0800

    列表render

--- staticfs/thirdparty/timeline/jquery.timeline.js
+++ staticfs/thirdparty/timeline/jquery.timeline.js
@@ -8,8 +8,9 @@
 			size:'3',  //每栏显示的默认最大篇数
 			leftCont:$('.leftnav'),  //时间轴
 			container:$('.content'),
-			template:'<div></div>'
-			rightCont:$('.blogs')  //具体内容展示区
+			template:'<div></div>',
+			rightCont:$('.blogs'),  //具体内容展示区
+			// getblogs:  获取文章列表接口
 		}
 		this.obj = obj;		//当前对象
 		this.options = $.extend({},defaults,options);
@@ -43,44 +44,134 @@
 		mouseEvent:function(){
 			//主要是滚动
 			//包括懒加载和侧边栏的滚动
-			this.options.container.on('scroll',function(){
-				console.log('scroll');
+			var that = this;
+			$(window).on('scroll',function(){
+				that.scrollFunc();
 			})
 		},
-		mouseScroll:function(event){
-			//鼠标滚动事件，判断是否开始进行加载，加载栏数、篇数、包括render、事件绑定
+		scrollFunc:function(event){
+			//鼠标滚动事件，判断是否开始进行加载，加载栏数、篇数、包括render、事件绑定(去掉)
+			//事件执行函数
+			var that = this;
+			if(this.options.getblogs){
+				//获取文章列表
+				this.loadBlogs();
+			}
 
 		},
 		monthDetail:function(){
 			//显示当前年／月包含的具体月／日
 			// 监听滚动事件
 		},
-		loadBlogs:function(){
+		loadBlogs:function(callback){
 			//加载博客
-			var data ={
-				title:'这是第一篇博客',
-				time:'5/04:44',
-				like:'30',
-				read:'1000',
-				thumb:'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png'
-			};
-			return data;
+			var that = this;
+			$.ajax({
+				type:'GET',
+				url:that.options.getblogs,
+				crossDomain: true,
+				// dataType:'jsonp',
+				// jsonpCallback:'getArticles',
+				data:null,
+				// contentType: "application/json;utf-8", 
+				success:function(data){
+					// data = JSON.parse(that.base64decode(data));
+					for(var i =0 ;i<data.articles.length;i++){
+						that.blogShows(data.articles[i]);	
+					}
+				},
+				error:function(error){
+					console.log(error);
+				}
+			});
+			// var data ={
+			// 	title:'这是第一篇博客',
+			// 	time:'5/04:44',
+			// 	like:'30',
+			// 	read:'1000',
+			// 	thumb:'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png'
+			// };
+			// that.blogShows(data);
+			// return data;
 		},
-		renderBlog:function(){
-
+		renderTemplate:function(template,data){  //待优化
+			//自定义模版引擎
+			//<%%> 模版的逻辑表达式
+			//<%=%> 输出表达式 同_.underscore里面的值
+			var reg =  /<%=\s*([^%>]+\S)\s*%>/;
+			var match;
+			//匹配不到则为null,循环则停止
+	        while(match = reg.exec(template)){//exec匹配字符串中的正则表达式
+	        	if(data[match[1]]!='undefined'){
+	        		//替换
+		         	template = template.replace(match[0],data[match[1]]);	
+	        	}
+	        	// else {}
+		         
+		      }
+		      return template;//返回渲染值
 		},
 		blogsEvent:function(){
 			//每篇博客上的事件绑定
 		},
-		blogTemplate:function(data){
+		blogShows:function(data){
 			//统一的博客展示模版
+			var that = this;
 			var template = this.options.template;
-			var t = _.template(template,{thumb:data.thumb,title:data.title,time:data.time,read:data.read});
+			var t = that.renderTemplate(template,{thumb:data.thumb,title:data.title,time:data.updated_at,read:data.hits});
 			this.options.container.append(t);
 		},
 		redrawTime:function(){
 			//重绘左侧的时间轴
-		}
+		},
+		base64decode:function(str){
+			var base64DecodeChars = new Array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
+		    var c1, c2, c3, c4;
+		    var i, len, out;
+		    len = str.length;
+		    i = 0;
+		    out = "";
+		    while (i < len) {
+		        /* c1 */
+		        do {
+		            c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+		        }
+		        while (i < len && c1 == -1);
+		        if (c1 == -1) 
+		            break;
+		        /* c2 */
+		        do {
+		            c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+		        }
+		        while (i < len && c2 == -1);
+		        if (c2 == -1) 
+		            break;
+		        out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+		        /* c3 */
+		        do {
+		            c3 = str.charCodeAt(i++) & 0xff;
+		            if (c3 == 61) 
+		                return out;
+		            c3 = base64DecodeChars[c3];
+		        }
+		        while (i < len && c3 == -1);
+		        if (c3 == -1) 
+		            break;
+		        out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+		        /* c4 */
+		        do {
+		            c4 = str.charCodeAt(i++) & 0xff;
+		            if (c4 == 61) 
+		                return out;
+		            c4 = base64DecodeChars[c4];
+		        }
+		        while (i < len && c4 == -1);
+		        if (c4 == -1) 
+		            break;
+		        out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+		    }
+		    return out;
+}
 	};
 	$.fn.timeline = function(options){
 		// options = $.extend({},defaults,options);
